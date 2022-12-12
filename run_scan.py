2faf6deb2202cc775f147a599242210f8179b74d
mjadiaz <jmauricio.adiaz@gmail.com>
2022-12-12 17:30:12 +0000

    rewrite using bsm_model style

--- run_scan.py
+++ run_scan.py
@@ -1,31 +1,8 @@
-from src.ray_scan import *
-from omegaconf import OmegaConf
+from src.utils import run_scan
+from src.ray_scan import SCANNER_DEFAULT_CONFIG
+from src.ray_scan import HEP_DEFAULT_CONFIG
 
-import numpy as np
-import os 
-import ray
-
-cwd = os.getcwd()
-scan_dir = os.path.join(cwd, 'test')
-
-config = OmegaConf.load('hep_tools.yaml')
-config.directories.scan_dir = scan_dir
-#config.scanner.n_workers = 2
-#config.scanner.max_sampels = 10
-
-
-ray.init(local_mode=False)
-
-scanner = Scanner.remote(config = config)
-reporter = Reporter.remote(scanner)
-remote_samplers = [RemoteSampler.remote(scanner, i, config) for i in range(config.scanner.n_workers)]
-
-processes = []
-for remote_sampler in remote_samplers:
-	processes.append(remote_sampler)
-processes.append(reporter)
-
-processes = [p.run.remote() for p in processes]
-ray.wait(processes)
-
-
+run_scan(
+        SCANNER_DEFAULT_CONFIG,
+        HEP_DEFAULT_CONFIG
+        )
